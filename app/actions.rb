492ce177f56774bc40c04cad53be793cab387839
TanveetGill <tanveet.gill@gmail.com>
2016-06-01 12:59:35 -0400

    new features and updates

--- app/actions.rb
+++ app/actions.rb
@@ -3,13 +3,28 @@ require 'rubygems'
 require 'sinatra'
 require "sinatra/activerecord"
 
+get '/' do
+	erb :index
+end
+
 get '/contacts' do
 	content_type :json
 	@contacts = Contact.all
 	return @contacts.to_json
-	erb :index
 end
 
 post '/contacts' do
+	content_type :json
+	@contact = Contact.new
+	@contact.firstname = params[:firstname]
+	@contact.lastname = params[:lastname]
+	@contact.number = params[:number]
+	@contact.save
+end
+
+get '/contacts/delete/:id' do
+	content_type :json
+	@contact = Contact.find_by(params[:id])
+	return @contact.to_json
 	
 end
